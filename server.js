2f6900788bbfc9a2ceb9e76d11416112e59f1509
BauntyL <bauntyxr@mail.ru>
2025-05-24 23:54:02 +0400

    Fix server.js path for Render deployment

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 // Перенаправление для совместимости с Render.com
-import('./dist/index.js');
+import('./dist/index.js').catch(() => {
+  // Fallback если dist не найден
+  import('./server/index.js');
+});
